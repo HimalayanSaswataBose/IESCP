f7c2990ee405c1ddcb6cab3c2b5737f462eb2349
Saswata Bose <67733706+HimalayanSaswataBose@users.noreply.github.com>
2024-08-08 19:49:50 +0530

    Update app.py

--- MAD1_Project/app.py
+++ MAD1_Project/app.py
@@ -23,7 +23,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS negotiations ( negotiator TEXT, negot
 connection.commit()
 cursor.execute("CREATE TABLE IF NOT EXISTS ongoing_ad ( sponsor TEXT, influencer TEXT, campaignName TEXT, adName TEXT, stipend TEXT, UNIQUE (sponsor, influencer, campaignName, adName))")
 connection.commit()
-cursor.execute("CREATE TABLE IF NOT EXISTS log_influencer ( date TEXT, influencer TEXT, activity TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS log_influencer ( date TEXT, influencer TEXT, activity TEXT, UNIQUE(data))")
 connection.commit()
 cursor.execute("CREATE TABLE IF NOT EXISTS log_sponsor ( date TEXT, sponsor TEXT, activity TEXT, UNIQUE(date))")
 connection.commit()
